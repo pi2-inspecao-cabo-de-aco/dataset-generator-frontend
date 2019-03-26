b7613323546698f414acb1e8c38aae1b64953c30
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-03-26 01:10:15 -0300

    Adding uploader and select input to index and adjusting styles

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   q-layout(view="hHh lpR lFf")
 
-    q-header(reveal).bg-light-blue-10.text-white
+    q-header(reveal).bg-primary.text-white
       q-toolbar
-        q-btn(dense flat round icon="menu" @click="left = !left").no-shadow
+        q-btn(dense flat round icon="menu" @click="left = !left").no-shadow.cursr-pointer
 
         q-toolbar-title
+          q-icon(name="mdi-database-plus").q-mr-sm
           span Dataset Generator
 
     q-drawer(v-model="left" side="left" bordered)
@@ -35,6 +36,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,64 @@
 <template lang="pug">
-  q-page.flex.flex-center.bg-grey-1
-</template>
+  q-page.index.column.flex-center.bg-secondary
+    q-uploader(
+      url="http://localhost:5000/upload"
+      label="Carregue arquivos para gerar o Dataset"
+      color="primary"
+      multiple
+    ).no-shadow
 
-<style>
-</style>
+    q-select(
+      standout
+      v-model="model"
+      :options="options"
+      label="Rótulo para as imagens carregadas"
+      options-dense
+      stack-label
+      bg-color="primary"
+      color="white"
+      dark
+    ).q-mt-lg
+
+    q-btn(
+      color="accent"
+      no-caps
+      unelevated
+    ).q-mt-lg Gerar CSV
+</template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      model: null,
+      options: [
+        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+@import '~quasar-variables'
+.index
+  .q-uploader
+    min-width 500px
+    min-height 200px
+    background $secondary
+
+  .q-select
+    min-width 500px
+</style>
+
+<style lang="stylus">
+@import '~quasar-variables'
+.index
+  .q-uploader__list
+    background #1c2633
+
+  .q-field--dark.q-field--focused
+    .q-field__native
+      color white !important
+</style>
